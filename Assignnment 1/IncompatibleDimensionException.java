88544df83c1557882713cc693f0193d9648d91b0
ppmurmu <pradeeppetermurmu@gmail.com>
2020-05-27 15:16:21 +0530

    Matrix Manipulations using TwoDBlockMatrix

---
+++ Assignnment 1/IncompatibleDimensionException.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.*;
+import java.io.*;
+class IncompatibleDimensionException extends Exception
+{
+    private static final long serialVersionUID = 1L;
+    public IncompatibleDimensionException()
+    {
+    }
+}
---
+++ Assignnment 1/SubBlockNotFoundException.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.*;
+import java.io.*;
+class SubBlockNotFoundException extends Exception
+{
+    private static final long serialVersionUID = 1L;
+    public SubBlockNotFoundException()
+    {
+    }
+}
